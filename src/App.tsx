1ee0fc4b4ecba942ae429bf9cf069c7af1ebceaf
Joe Dearman <joedearman@gmail.com>
2024-12-05 16:23:09 -0500

    Update App with ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ChatInterface from './components/ChatInterface';
+import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
-  return <ChatInterface />;
+  return (
+    <ThemeProvider>
+      <ChatInterface />
+    </ThemeProvider>
+  );
 }
 
 export default App;
